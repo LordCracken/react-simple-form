51add251b428a4e3cbe6ce1045cd54d4fafd3eec
LordCracken <vladislav070201@gmail.com>
2022-11-02 02:22:55 +0300

    add simple validation for name input

--- src/components/SimpleInput.tsx
+++ src/components/SimpleInput.tsx
@@ -1,9 +1,36 @@
+import { useState, useRef, ChangeEvent, FormEvent } from 'react';
+
 const SimpleInput = () => {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const [enteredName, setEnteredName] = useState<string>('');
+
+  const nameInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') return;
+
+    const enteredValue = nameInputRef.current?.value ?? null;
+    console.log(enteredName);
+    console.log(enteredValue);
+
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          ref={nameInputRef}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
